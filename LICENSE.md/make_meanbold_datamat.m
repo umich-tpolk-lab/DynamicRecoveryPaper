a77ae1b540ae40d6effd00147438f72d22899b42
Poortata <46610416+Poortata@users.noreply.github.com>
2021-06-27 10:45:27 -0500

    Delete LICENSE.md directory

--- LICENSE.md/make_meanbold_datamat.m
+++
@@ -1,7 +0,0 @@
-function make_meanbold_datamat(path,filename)
-
-runname = (filename); 
-cd (path) % The toolbox outputs our mean-BOLD PLS files in the current directory
-batch_plsgui(runname);
-
-end
